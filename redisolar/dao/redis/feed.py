c28373c04ff565a86c44aaa14ea78201eb2d6541
Rupesh Yadav <rupeshyadav11101999@gmail.com>
2024-01-18 19:39:06 +0530

    Challenge 6

--- redisolar/dao/redis/feed.py
+++ redisolar/dao/redis/feed.py
@@ -28,6 +28,12 @@ class FeedDaoRedis(FeedDaoBase, RedisDaoBase):
                 pipeline: redis.client.Pipeline) -> None:
         """Helper method to insert a meter reading."""
         # START Challenge #6
+        pipeline.xadd(self.key_schema.global_feed_key(), 
+                      MeterReadingSchema().dump(meter_reading), 
+                      maxlen=self.GLOBAL_MAX_FEED_LENGTH)
+        pipeline.xadd(self.key_schema.feed_key(meter_reading.site_id),
+                      MeterReadingSchema().dump(meter_reading),
+                      maxlen=self.SITE_MAX_FEED_LENGTH)
         # END Challenge #6
 
     def get_recent_global(self, limit: int, **kwargs) -> List[MeterReading]:
