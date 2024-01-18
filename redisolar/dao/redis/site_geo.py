c1d68beefc3e5693557dbb4df2cc2d8f1076933a
Rupesh Yadav <rupeshyadav11101999@gmail.com>
2024-01-18 17:06:46 +0530

    Challenge 5

--- redisolar/dao/redis/site_geo.py
+++ redisolar/dao/redis/site_geo.py
@@ -55,6 +55,10 @@ class SiteGeoDaoRedis(SiteGeoDaoBase, RedisDaoBase):
     def _find_by_geo_with_capacity(self, query: GeoQuery, **kwargs) -> Set[Site]:
         # START Challenge #5
         # Your task: Get the sites matching the GEO query.
+        site_ids = self.redis.georadius(
+            self.key_schema.site_geo_key(), query.coordinate.lng, query.coordinate.lat,
+            query.radius, query.radius_unit.value
+        )
         # END Challenge #5
 
         p = self.redis.pipeline(transaction=False)
@@ -63,15 +67,18 @@ class SiteGeoDaoRedis(SiteGeoDaoBase, RedisDaoBase):
         #
         # Your task: Populate a dictionary called "scores" whose keys are site
         # IDs and whose values are the site's capacity.
-        #
+        capacity_ranking_key = self.key_schema.capacity_ranking_key()
+        for site_id in site_ids:
+            p.zscore(capacity_ranking_key, site_id)
+        scores = dict(zip(site_ids, p.execute()))
         # Make sure to run any Redis commands against a Pipeline object
         # for better performance.
         # END Challenge #5
 
         # Delete the next lines after you've populated a `site_ids`
         # and `scores` variable.
-        site_ids: List[str] = []
-        scores: Dict[str, float] = {}
+        # site_ids: List[str] = []
+        # scores: Dict[str, float] = {}
 
         for site_id in site_ids:
             if scores[site_id] and scores[site_id] > CAPACITY_THRESHOLD:
@@ -91,9 +98,19 @@ class SiteGeoDaoRedis(SiteGeoDaoBase, RedisDaoBase):
         site_ids = self.redis.zrange(self.key_schema.site_geo_key(), 0, -1)
         sites = set()
 
+        # for site_id in site_ids:
+        #     key = self.key_schema.site_hash_key(site_id)
+        #     site_hash = self.redis.hgetall(key)
+        #     sites.add(FlatSiteSchema().load(site_hash))
+
+        # Optional Bonus Challenge: 4.5
+        # Rewrite above code using pipeline
+        p = self.redis.pipeline(transaction=False)
         for site_id in site_ids:
             key = self.key_schema.site_hash_key(site_id)
-            site_hash = self.redis.hgetall(key)
+            p.hgetall(key)
+        site_hashes = p.execute()
+        for site_hash in site_hashes:
             sites.add(FlatSiteSchema().load(site_hash))
 
         return sites
