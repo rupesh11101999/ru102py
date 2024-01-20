974f8c7e784c25138a433a22ee457c737439de87
Rupesh Yadav <rupeshyadav11101999@gmail.com>
2024-01-20 16:59:08 +0530

    Replaced metrics with metricsTimeseries

--- redisolar/dao/redis/sliding_window_rate_limiter.py
+++ redisolar/dao/redis/sliding_window_rate_limiter.py
@@ -1,13 +1,13 @@
 # Uncomment for Challenge #7
-#import datetime
-#import random
+import datetime
+import random
 from redis.client import Redis
 
 from redisolar.dao.base import RateLimiterDaoBase
 from redisolar.dao.redis.base import RedisDaoBase
 from redisolar.dao.redis.key_schema import KeySchema
 # Uncomment for Challenge #7
-#from redisolar.dao.base import RateLimitExceededException
+from redisolar.dao.base import RateLimitExceededException
 
 
 class SlidingWindowRateLimiter(RateLimiterDaoBase, RedisDaoBase):
@@ -25,4 +25,16 @@ class SlidingWindowRateLimiter(RateLimiterDaoBase, RedisDaoBase):
     def hit(self, name: str):
         """Record a hit using the rate-limiter."""
         # START Challenge #7
+        key = KeySchema().sliding_window_rate_limiter_key(name, self.window_size_ms, self.max_hits)
+        now = datetime.datetime.utcnow().timestamp() * 1000
+
+        pipeline = self.redis.pipeline()
+        member = now + random.random()
+        pipeline.zadd(key, {member: now})
+        pipeline.zremrangebyscore(key, 0, now - self.window_size_ms)
+        pipeline.zcard(key)
+        _, _, hits = pipeline.execute()
+
+        if hits > self.max_hits:
+            raise RateLimitExceededException
         # END Challenge #7
